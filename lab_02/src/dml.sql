09ed0fde0ad336b2b638cb6cef2946a7b8708c2e
agarmir0920 <tsarevantuan@yandex.ru>
2022-10-05 23:10:27 +0300

    Added new queries

--- lab_02/src/dml.sql
+++ lab_02/src/dml.sql
@@ -1,22 +1,22 @@
--- 1. Программы с числом сотрудников более 500
+-- 1. Инструкция SELECT, использующая предикат сравнения
 select name, country, workers_count
 from space_programs
 where workers_count > 500
 order by workers_count;
 
--- 2. Астрономические объекты, открытые между 1950 и 1970 годами
+-- 2. Инструкция SELECT, использующая предикат BETWEEN
 select distinct ao.astro_object_name, ao.discovery_year, glx.name
 from astro_objects ao inner join galaxies glx on ao.galaxy_id = glx.id
 where ao.discovery_year between 1950 and 1970
 order by ao.discovery_year;
 
--- 3. Космонавты, в фамилиях которых присутствует окончание 'son'
+-- 3. Инструкция SELECT, использующая предикат LIKE
 select name, birth_year
 from cosmonauts
 where name like '% %son'
 order by name;
 
--- 4. Спутники, поддерживаемые программой id = 641
+-- 4. Инструкция SELECT, использующая предикат IN с вложенным подзапросом
 select stlt.stlt_name, stlt.stlt_type, stlt.astro_object_id
 from satellites stlt
 where stlt.id in (select satellite_id
@@ -24,7 +24,7 @@ where stlt.id in (select satellite_id
                   where space_program_id = 641)
 order by stlt.stlt_name;
 
--- 5. Список неподдерживаемых спутников (космический мусор)
+-- 5. Инструкция SELECT, использующая предикат EXISTS с вложенным подзапросом
 select stlt_name, stlt_type, astro_object_id
 from satellites
 where not exists (select satellite_id
@@ -32,7 +32,7 @@ where not exists (select satellite_id
                   where satellite_id = satellites.id)
 order by stlt_name;
 
--- 6. Самые молодые космонавты в каждой программе
+-- 6. Инструкция SELECT, использующая предикат сравнения с квантором
 select csm1.name, csm1.birth_year, csm1.space_program_id
 from cosmonauts csm1
 where csm1.birth_year >= all (select csm2.birth_year
@@ -41,21 +41,21 @@ where csm1.birth_year >= all (select csm2.birth_year
                               and csm1.id != csm2.id)
 order by csm1.space_program_id;
 
--- 7. Средний бюджет программ по странам
+-- 7. Инструкция SELECT, использующая агрегатные функции в выражениях столбцов
 select country, round(avg(budget)) avg_budget, round(sum(budget) / count(country)) truth_avg
 from (select country, budget
       from space_programs) country_total_budget
 group by country
 order by country;
 
--- 8. Средняя масса космонавтов по программам
+-- 8. Инструкция SELECT, использующая скалярные подзапросы в выражениях столбцов
 select name,
 (select round(avg(mass))
 from cosmonauts csm1
 where csm1.space_program_id = csm2.space_program_id) prog_avg_mass
 from cosmonauts csm2;
 
--- 9. Поиск российских программ
+-- 9. Инструкция SELECT, использующая простое выражение CASE
 select name,
 case country
     when 'Russia' then 'Отечественная'
@@ -64,7 +64,7 @@ end is_russian
 from space_programs
 order by is_russian desc;
 
--- 10. Поиск спутников, запущенных сразу после создания
+-- 10. Инструкция SELECT, использующая поисковое выражение CASE
 select stlt_name,
 case
     when launch_year = creation_year then 'Запущен сразу'
@@ -73,8 +73,45 @@ end fast_launch
 from satellites
 order by fast_launch desc;
 
--- 11. Временная таблица с информацией о линзообразных галактиках
+-- 11. Создание новой временной локальной таблицы из результирующего набора данных инструкции SELECT
 --select *
 --into temp lenticular_galaxy
 --from galaxies
 --where galaxy_type = 'Линзообразная';
+
+-- 12. Инструкция SELECT, использующая вложенные коррелированные подзапросы в качестве производных таблиц в предложении FROM.
+select tall_cosmonauts.name, tall_cosmonauts.growth, sp.name
+from space_programs sp inner join (select id, name, growth, space_program_id
+                                   from cosmonauts
+                                   where growth >= 190)
+                                   as tall_cosmonauts on tall_cosmonauts.space_program_id = sp.id
+order by tall_cosmonauts.growth, tall_cosmonauts.name;
+
+-- 13. Инструкция SELECT, использующая вложенные подзапросы с уровнем вложенности 3
+select *
+from galaxies
+where id in (select galaxy_id
+             from astro_objects ao
+             where ao.id not in (select distinct stlt.astro_object_id
+                                 from satellites stlt
+                                 where stlt.astro_object_id = ao.id));
+
+-- 14. Инструкция SELECT, консолидирующая данные с помощью предложения GROUP BY, но без предложения HAVING
+select country, round(avg(growth)) as avg_growth
+from cosmonauts csm inner join space_programs sp on csm.space_program_id = sp.id
+group by country
+order by country;
+
+-- 15. Инструкция SELECT, консолидирующая данные с помощью предложения GROUP BY и предложения HAVING
+select country, round(avg(growth)) as avg_growth
+from cosmonauts csm inner join space_programs sp on csm.space_program_id = sp.id
+group by country
+having avg(growth) > 175
+order by country;
+
+-- 16. Однострочная инструкция INSERT, выполняющая вставку в таблицу одной строки значений
+insert into prog_stlt_rel (space_program_id, satellite_id)
+values (681, 1);
+
+-- 17. Многострочная инструкция INSERT, выполняющая вставку в таблицу результирующего набора данных вложенного подзапроса
+
