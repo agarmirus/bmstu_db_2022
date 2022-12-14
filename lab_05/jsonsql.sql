fded419b89be61a0271b3f607cb131ee794d5cc8
agarmirus <tsarevantuan@yandex.ru>
2022-12-14 13:23:56 +0300

    Written some

--- lab_05/jsonsql.sql
+++ lab_05/jsonsql.sql
@@ -24,3 +24,36 @@ create temp table if not exists cosm_json(d json);
 copy cosm_json from '/data/c.json';
 
 select * from cosm_json;
+
+--select * from cosm_json, json_populate_record(null::cosm, d);
+--
+--select json_populate_record(null::cosm, d)
+--from cosm_json;
+
+insert into cosm
+select id, name, birth_year, mass, growth, space_program_id
+from cosm_json, json_populate_record(null::cosm, d);
+
+select * from cosm;
+
+-- 3. Таблица с json
+insert into cosm_json
+select * from json_build_object('id', 1001, 'name', 'Bruh Bruhovych', 'birth_year', 1999, 'mass', 99, 'growth', 199, 'space_program_id', null);
+
+select * from cosm_json;
+
+-- 4.
+-- 4.1
+select * from json_extract_path_text((select * from cosm_json where json_extract_path_text(d, 'id') = '998'), 'name');
+
+-- 4.2
+select d->'name' as name
+from cosm_json;
+
+-- 4.3
+select (d->'id')::text::int as id
+from cosm_json
+where (d->'id')::text::int = 2000;
+
+-- 4.4
+
